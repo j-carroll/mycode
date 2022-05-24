4f7969e8b437f93b93961326d096fc0df85b10d5
j-carroll <jrcarr2@gmail.com>
2022-05-24 18:43:52 +0000

    learning about input functions

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+
+    # take user input from stdin
+    user_input = input("Please enter an IPv4 IP Address:")
+
+    # display ip address
+    print("Your IP Address is: " + user_input)
+
+# this calls main function
+main()
