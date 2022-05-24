1c0ffe66bbde1bc1e1b3f81111edbb8d095afe88
j-carroll <jrcarr2@gmail.com>
2022-05-24 19:41:32 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+# List that includes IP address, port and status
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# Print the ip address
+print("The first item in the list (IP): " + my_list[0] )
+
+# Print the port as a string
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# Print the status of the connection
+print("The last item in the list (state): " + my_list[2] )
---
+++ mix-list/mixlist02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+# list ov several ports, IPs and a protocl
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# Display only IP addresses
+print("These are the only IP addreses: ", iplist[3], iplist[4] )
