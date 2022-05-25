326f8f612272b314e56a940ffd767b1bf8980d9a
j-carroll <jrcarr2@gmail.com>
2022-05-25 19:57:12 +0000

    studying for logic

---
+++ forloop.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+for i in range(14):
+    print(i, end=" ")
---
+++ introfor/challenge1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for farm in farms:
+    print(farm)
